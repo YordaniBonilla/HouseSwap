eb7ea70b35d0ab283e17744c96c6df9a4444d00b
YordaniBonilla <b.yordani93@gmail.com>
2019-05-17 08:04:38 -0500

    reference child keys to parent table

--- server/schema.sql
+++ server/schema.sql
@@ -1,27 +1,29 @@
 DROP DATABASE IF EXISTS Users;
-
+/* if database exists then delete it*/
 CREATE DATABASE Users;
-
+/*creates Users database */
 USE Users;
-
-CREATE TABLE Countries (
+/* tell the system what database to use*/
+/*then we create a table in database and poppulate it*/
+CREATE TABLE countries (
 id int NOT NULL AUTO_INCREMENT, 
 country varchar(20)
 ); 
 
 CREATE TABLE Users (
   id int NOT NULL AUTO_INCREMENT,
-  name varchar(50),
+  username varchar(50),
   about varchar(750),
-  location int 2 ,
+  userlocation int 2 ,
   destination int 2,
   picture varchar(50),
   email varchar(40),
   phone varchar(20),
   twitter varchar(50),
   facebook varchar(60),
-  username varchar(50),
-  foreign key(location) references countries(id)
+  username varchar(50), 
+  foreign key(userlocation) references countries(id),
+  foreign key(destination) references countries(id)
 );
 
 INSERT INTO Countries (country) VALUE ("Antartica
